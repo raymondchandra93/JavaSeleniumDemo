b0537fc13fa31ebc1dff8fab37486b4be086a073
Raymond Chandra <raymond.chandra@fdmgroup.com>
2024-05-23 17:23:10 -0400

    V 1.0.1 - Implementing basic applitools comparison and baseline

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -1,47 +1,70 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.applitools.eyes.RectangleSize;
+import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.selenium.fluent.Target;
+
 public class TestCase {
 	WebDriver driver;
+	Eyes eyes;
+	static Properties config;
+	
+	@BeforeAll
+	public static void beforeAll() {
+		config = new Properties();
+		String driverName = "";
 
+		// Get the properties file
+		try {
+			config.load(new FileInputStream("src/test/resources/config.properties"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@BeforeEach
-	public void beforeEach() {
+	public void beforeEach(TestInfo testInfo) {
+
+		eyes = new Eyes();
+		eyes.setApiKey(config.getProperty("API_KEY"));
 		driver = new ChromeDriver();
+		
+		eyes.open(
+			driver,
+			"My First Tests",
+			testInfo.getTestMethod().get().getName(),
+			new RectangleSize(1000, 600)
+		);
 	}
 
 	@Test
-    public void myTestCase() {
-        driver.get("https://applitools.com/helloworld/");
-        WebElement numbers = driver.findElement(By.cssSelector("span.primary"));
-        WebElement button = driver.findElement(By.cssSelector("div.section:nth-child(3) > button:nth-child(1)"));
-        WebElement titleH = driver.findElement(By.cssSelector("div.fancy:nth-child(1) > span:nth-child(1)"));
-        WebElement titleD = driver.findElement(By.cssSelector("div.fancy:nth-child(1) > span:nth-child(11)"));
-
-        Assertions.assertEquals(numbers.isDisplayed(), true);
-        Assertions.assertEquals(numbers.getText(), "123456");
-        Assertions.assertEquals(numbers.getCssValue("color"), "rgba(78, 90, 99, 1)");
-
-        Assertions.assertEquals(button.isDisplayed(), true);
-        Assertions.assertEquals(button.getText(), "Click me!");
-        Assertions.assertEquals(button.getCssValue("color"), "rgba(255, 255, 255, 1)");
-
-        Assertions.assertEquals(titleH.isDisplayed(), true);
-        Assertions.assertEquals(titleH.getText(), "H");
-        Assertions.assertEquals(titleH.getCssValue("color"), "rgba(255, 0, 0, 1)");
-
-        Assertions.assertEquals(titleD.isDisplayed(), true);
-        Assertions.assertEquals(titleD.getText(), "D");
-        Assertions.assertEquals(titleD.getCssValue("color"), "rgba(70, 0, 255, 1)");
+    public void testApplitoolsWebWindow() {
+        // driver.get("https://applitools.com/helloworld/");
+        driver.get("https://applitools.com/helloworld/?diff1");			// Changing the baseline
+        
+        // Checking whether the window is there or not
+        eyes.check(Target.window());
     }
 
 	@AfterEach
 	public void afterEach() {
+		eyes.closeAsync();;
 		driver.close();
 	}
 }
